69c78f9b5af83db73b461b7450ba3b84f64b677c
CDZk1 <kauacardosogoncalves111@gmail.com>
2026-05-25 20:54:38 -0300

    questões

--- MaiorValorDistinto/main.c
+++ MaiorValorDistinto/main.c
@@ -2,11 +2,28 @@
 
 int main(){
 
-    int numero;
+    int tamanho, maior = -2147483648, menor = 2147483647;
 
-    printf("Digite o tamanho do vetor: ");
-    scanf("%d", &numero);
-    
+    printf("informe o tamanho do vetor: ");
+    scanf("%d", &tamanho);
+
+    int vetor[tamanho];
+
+    for(int i = 0; i < tamanho; i++){
+        printf("Informe o numero %d: ", i + 1);
+        scanf("%d", &vetor[i]);
+
+        if(vetor[i] > maior){
+            maior = vetor[i];
+        }
+
+        if(vetor[i] < menor){
+            menor = vetor[i];
+        }
+    }
+    printf("Os numeros salvos no vetor de tamanho %d\n", tamanho);
+    printf("O maior numero informado foi %d\n", maior);
+    printf("O menor numero informado foi %d\n", menor);
 
     return 0;
 }
