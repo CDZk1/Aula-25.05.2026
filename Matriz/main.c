1b31339718bc6a331c1c27fdbd5275ad691a0974
CDZk1 <kauacardosogoncalves111@gmail.com>
2026-05-25 21:51:39 -0300

    questões

--- Matriz/main.c
+++ Matriz/main.c
@@ -9,7 +9,41 @@
 #include <stdio.h>
 
 int main(){
-    
+    int tamanho;
+
+    printf("Digite o tamanho da matriz: ");
+    scanf("%d", &tamanho);
+
+    int matriz[tamanho][tamanho], d_principal = 0, d_secundaria, t_superior, t_inferior;
+
+    printf("Digite os numeros da matriz: \n");
+    for(int i = 0; i < tamanho; i++){
+        for(int j = 0; j < tamanho; j++){
+            printf("Informe o elemento [%d][%d]", i, j);
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+    for(int i = 0; i < tamanho; i++){
+        for(int j = 0; j < tamanho; j++){
+            if(i == j){
+            d_principal += matriz[i][j];
+            }
+            if(j > i){
+                t_superior += matriz[i][j];
+            }
+            if(i > j){
+                t_inferior += matriz[i][j];
+            }
+            if(i + j == tamanho - 1){
+                d_secundaria += matriz[i][j];
+            }
+            
+        }
+    }
+    printf("A soma da diagonal principal e: %d\n", d_principal);
+    printf("A soma da triangular superior e: %d\n", t_superior);
+    printf("A soma da triangular inferior e: %d\n", t_inferior);
+    printf("A soma da diagonal secundaria e: %d\n", d_secundaria);
 
     return 0;
 }
